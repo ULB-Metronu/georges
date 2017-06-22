d3afee9048f66fb4fc91cdcc57385f200b2bafdc
chernals <cedric.hernalsteens@cern.ch>
2017-06-22 20:15:50 +0200

    Plots from a survey file (experimental and relatively ugly)

---
+++ plotting/survey.py
@@ -0,0 +1,27 @@
+def survey(ax, transmission):
+    """Draw a minimalistic survey overview of the beamline."""
+    ax2.get_xaxis().set_tick_params(direction='out')
+    ax2.yaxis.set_ticks_position('left')
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.yaxis.set_ticks_position('right')
+    ax2.set_xlim([ticks_locations[0], ticks_locations[-1]])
+    ax2.tick_params(axis='x', labelsize=6)
+    ax2.xaxis.set_major_formatter(FixedFormatter([]))
+    ax2.xaxis.set_major_locator(FixedLocator(ticks_locations))
+    ax2.yaxis.set_major_locator(MultipleLocator(25))
+    ax2.set_ylabel('T ($\%$)')
+    ax2.yaxis.label.set_color(palette['green'])
+    ax2.set_ylim([0, 100])
+    ax2.grid(True)
+    ax2.plot(transmission, '^-', color=palette['green'])
+
+    ax.set_xlim([ticks_locations[0], ticks_locations[-1]])
+    ax.yaxis.set_major_locator(MultipleLocator(10))
+    ax.set_ylabel('Losses ($\%$)')
+    ax.yaxis.label.set_color(palette['magenta'])
+    ax.bar(transmission.index-0.125, -transmission.diff(), 0.125,alpha=0.7,
+            edgecolor=palette['magenta'],
+            color=palette['magenta'],
+            #yerr=transmission.apply(compute_losses_error),
+            error_kw=dict(ecolor=palette['base02'], lw=1, capsize=2, capthick=1))
+    ax.set_ylim([0, transmission.diff().abs().max()+5.0])
