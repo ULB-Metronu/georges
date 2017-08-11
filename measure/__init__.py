6717031eebc876763489bbe34abfb499d6eeaa0f
chernals <cedric.hernalsteens@cern.ch>
2017-08-11 15:55:10 +0200

    PCVue BPM data processing.

--- measure/__init__.py
+++ measure/__init__.py
@@ -1,1 +1,2 @@
 from .variquad import variquad
+from .bpm import bpm, bpm_plot
--- measure/bpm.py
+++ measure/bpm.py
@@ -1,10 +1,11 @@
-import re
+import re, os
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 
-DATA_BPM = {
+DATA_BPM_PCVUE = {
     'max_current': {
         'regex': re.compile("Max. current : (.*) nA/mm"),
     },
@@ -59,3 +60,61 @@ def bpm_fit(x, y):
     popt, pcov = curve_fit(gaussian, x, y, p0=[ar, mean, rms])
 
     return [popt, np.sqrt(pcov.diagonal())]
+
+
+def bpm(**kwargs):
+    """Process a PCVue BPM data file and read associated data onto a DataFrame.
+    :param kwargs: parameters are:
+        - path: the path were the data file and bpm data files can be found
+        - file: the data file name
+        - bpm: the BPM name
+        - instrument: the specific parsing data for the instrument
+    """
+    path = kwargs.get("path", ".")
+    file = kwargs.get("file") if kwargs.get("file", "").endswith(".csv") else kwargs.get("file") + '.csv'
+    bpm_name = kwargs.get("bpm", "BPM")
+    instrument = kwargs.get("instrument", DATA_BPM_PCVUE)
+    bpm_data = pd.read_csv(os.path.join(path, file), skiprows=0, encoding="utf-8-sig")
+    bpm_data["{}_data".format(bpm_name)] = bpm_data[bpm_name].apply(
+        lambda x: read_data_file(
+            "{0:04d}.txt".format(x),
+            instrument,
+            {'init': 'Y', 'next': 'X', 'trigger': "Y-Wire data"},
+            path
+        )
+    )
+    bpm_data["{}_fit_X".format(bpm_name)] = bpm_data["{}_data".format(bpm_name)].apply(
+        lambda x: bpm_fit(
+            x['channel']['data']['X'][:, 1],
+            x['channel']['data']['X'][:, 2]
+        )
+    )
+    bpm_data["{}_fit_Y".format(bpm_name)] = bpm_data["{}_data".format(bpm_name)].apply(
+        lambda x: bpm_fit(
+            x['channel']['data']['Y'][:, 1],
+            x['channel']['data']['Y'][:, 2]
+        )
+    )
+    bpm_data["{}_fit_sigma_X".format(bpm_name)] = bpm_data["{}_fit_X".format(bpm_name)].apply(lambda x: np.abs(x[0][2]))
+    bpm_data["{}_fit_sigma_Y".format(bpm_name)] = bpm_data["{}_fit_Y".format(bpm_name)].apply(lambda x: np.abs(x[0][2]))
+
+    return bpm_data
+
+
+def bpm_plot(r, bpm_name, **kwargs):
+    """Process a row of a BPM dataframe and returns a plot"""
+    if kwargs.get("ax"):
+        ax = kwargs.get("ax")
+    else:
+        ax = plt.figure().add_subplot(111)
+    if kwargs.get("axis"):
+        axis = kwargs.get("axis")
+    else:
+        raise Exception("'axis' keyword argument must be 'X' or 'Y'.")
+
+    x = r["{}_data".format(bpm_name)]['channel']['data'][axis][:, 1]
+    y = r["{}_data".format(bpm_name)]['channel']['data'][axis][:, 2]
+
+    ax.plot(x, y, 'b+:', label='data')
+    ax.plot(x, gaussian(x, *r["{}_fit_{}".format(bpm_name, axis)][0]), 'ro:', label='fit')
+    ax.legend()
