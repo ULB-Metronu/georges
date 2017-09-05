f71db58b78275605c8d3883ce2f984794389010c
Robin Tesse <robintesse@gmail.com>
2017-09-05 09:34:45 +0200

    Minot changes

--- g4beamline/grammar.py
+++ g4beamline/grammar.py
@@ -5,10 +5,12 @@ g4beamline_syntax = {
 
     'define_brho': "param Brho={{{{BRHO}}}}",
     'define_detector': "sample detector radius=31.5 format=ascii",
-    'add_detector': "detector z={} rename={}';",
     'define_physics': "physics QGSP_BIC",
     'define_world': "param worldMaterial=Vacuum",
     'keep_protons': "trackcuts keep=proton",
+    'start_command': "start x=0 y=0 z=0 initialZ=0 radiusCut=31.5",  # Define the equivalent of MaxAper in G4Beamline
+    'beam_input': "beam ascii file=input_beam.dat nEvents={}",
+
     'beam_start': "beam gaussian particle={{{{PARTICLE.lower()}}}}"
                   " beamX={} meanXp={} "
                   " beamY={} meanYp={} "
@@ -25,22 +27,22 @@ g4beamline_syntax = {
                   "fieldMaterial=Vacuum " 
                   "ironColor=1,0,0 " 
                   "kill=1 " 
-                  "openAperture=1 ", # Do not forget the whitespace
+                  "openAperture=1 ",  # Do not forget the whitespace
     'ccoll': "tubs {} "
-            "innerRadius={} "
-            "outerRadius=50 "
-            "length={} "
-            "material=W "
-            "color=1,1,0 "
-            "kill=1",
+             "innerRadius={} "
+             "outerRadius=50 "
+             "length={} "
+             "material=W "
+             "color=1,1,0 "
+             "kill=1",
     'rcoll': "box {} " 
-            "width=60 " 
-            "height=80 "
-            "length={} "
-            "material=Ni "
-            "color=1,1,0 "
-            "kill=1",
+             "width=60 " 
+             "height=80 "
+             "length={} "
+             "material=Ni "
+             "color=1,1,0 "
+             "kill=1",
 
     'tesselatedsolids': "tessellatedsolid {} kill=0 material={} file={}",
-    'place_solids':"place {} x={} y={} z={} rotation=X{},Y{},Z{}",
+    'place_solids': "place {} x={} y={} z={} rotation=X{},Y{},Z{}",
 }
