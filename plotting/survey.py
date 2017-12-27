d50b7a5b4a4e44898ef2049d29841ba41636bc01
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2017-12-27 21:12:22 +0100

    Refactoring of georges.plotting to get different styles of plots when plotting survey data.

--- plotting/survey.py
+++ plotting/survey.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def survey(ax, bl, **kwargs):
+def survey_style1(ax, bl, **kwargs):
     ax.set_xlabel("X (m)")
     ax.set_ylabel("Y (m)")
     ax.set_xlim([0, np.max(bl.line['X']) / 1000])
@@ -51,3 +51,50 @@ def survey(ax, bl, **kwargs):
                     )
                 )
     plt.axes().set_aspect('equal', 'datalim')
+
+
+def survey_style2(ax, bl):
+    tmp = -90
+    for index, row in bl.line.iterrows():
+        if row['KEYWORD'] == 'SBEND' or row['KEYWORD'] == 'MARKER':
+            if row['KEYWORD'] == 'SBEND':
+                r = row['L'] / row['ANGLE']
+                if row['ANGLE'] > 0:
+                    c = 'r'
+                elif row['ANGLE'] < 0:
+                    c = 'b'
+                else:
+                    c = 'gray'
+            if row['KEYWORD'] == 'MARKER':
+                r = 0.1
+                c = 'b'
+            theta = -row['THETA']
+            angle = -row['ANGLE'] / np.pi * 180.0
+            centre = [(row['Z'] - np.sin(theta) * r), -(row['X'] - np.cos(theta) * r)]
+            theta1 = min(tmp, tmp - angle)
+            theta2 = max(tmp, tmp - angle)
+            tmp = theta1 if angle > 0 else theta2
+            w = patches.Wedge(centre, r + 0.2, theta1, theta2, width=0.4, alpha=1, facecolor=c, ec='k')
+        if row['KEYWORD'] == 'DRIFT':
+            w = patches.Rectangle(
+                (
+                    row['Z'] - row['L'] * np.cos(row['THETA']) - 0.2 * np.sin(row['THETA']),
+                    -row['X'] + row['L'] * np.sin(row['THETA']) - 0.2 * np.cos(row['THETA'])
+                ),
+                row['L'],
+                0.4,
+                angle=np.degrees(-row['THETA']),
+                alpha=0.2, facecolor='y', ec='k', hatch='')
+        ax.add_patch(w)
+
+    # For automatic scaling of the plot
+    ax.plot([], [])
+
+
+def survey(ax, bl, style='style2', **kwargs):
+    if style == 'style1':
+        survey_style1(ax, bl, **kwargs)
+    elif style == 'style2':
+        survey_style2(ax, bl)
+    else:
+        raise Exception("Style not supported.")
