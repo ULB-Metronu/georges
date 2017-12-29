c43b31446d9e5d0561e2225b4d18d4a97df085a4
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2017-12-29 18:46:22 +0100

    Removed 'topas' from init.py as well.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from .beamline_builder import BeamlineBuilder
 from .context import Context
 from . import madx
 from . import bdsim
-from . import topas
 from . import g4beamline
 from . import experimental
 from . import physics
