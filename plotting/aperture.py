a45e9cfd276e4b089e789399649135a507cb6e79
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2018-01-26 18:17:24 +0100

    Implementing correct plotting of collimator apertures (also with values from the context).

--- plotting/aperture.py
+++ plotting/aperture.py
@@ -3,11 +3,17 @@ import numpy as np
 from .common import palette
 
 
-def xy_from_string(a, i):
-    if len(str(a).strip('[]').split(',')) >= int(i) + 1:
-        return float(str(a).strip('[]').split(',')[int(i)])
-    elif len(str(a).strip('[]').split(',')) > 0:
-        return float(str(a).strip('[]').split(',')[0])
+def xy_from_string(a, i, c):
+    def convert(x):
+        try:
+            converted = float(x)
+        except ValueError:
+            converted = float(c.get(x))
+        return converted
+    if len(str(a).strip('[]').strip('{}').split(',')) >= int(i) + 1:
+        return convert(str(a).strip('[]').strip('{}').split(',')[int(i)])
+    elif len(str(a).strip('[]').strip('{}').split(',')) > 0:
+        return convert(str(a).strip('[]').strip('{}').split(',')[0])
     else:
         return np.inf
 
@@ -53,24 +59,23 @@ def draw_quad(ax, e):
 
 
 def draw_coll(ax, e, plane):
-    if e['SLITS_PLANE'] == plane:
-        ax.add_patch(
-            matplotlib.patches.Rectangle(
-                (e['AT_ENTRY'], 1000 * e['APERTURE_UP'] * 0.5),  # (x,y)
-                e['LENGTH'],  # width
-                100,  # height
-              #  hatch='.', facecolor=palette['coll']
-            )
+    ax.add_patch(
+        matplotlib.patches.Rectangle(
+            (e['AT_ENTRY'], 1000 * e['APERTURE_UP']),  # (x,y)
+            e['LENGTH'],  # width
+            100,  # height
+            facecolor=palette['coll']
         )
+    )
 
-        ax.add_patch(
-            matplotlib.patches.Rectangle(
-                (e['AT_ENTRY'], -1000 * e['APERTURE_DOWN'] * 0.5),  # (x,y)
-                e['LENGTH'],  # width
-                -100,  # height
-                hatch='.', facecolor=palette['coll']
-            )
+    ax.add_patch(
+        matplotlib.patches.Rectangle(
+            (e['AT_ENTRY'], -1000 * e['APERTURE_DOWN']),  # (x,y)
+            e['LENGTH'],  # width
+            -100,  # height
+            facecolor=palette['coll']
         )
+    )
 
 
 def draw_bend(ax, e):
@@ -96,12 +101,12 @@ def draw_bend(ax, e):
 def fill_aperture(element, context):
     if element.name+'_APERTURE' in context and element['TYPE'] == 'SLITS':
         element['APERTURE'] = context[element.name+'_APERTURE']
+    if element['TYPE'] == 'COLLIMATOR' and element['PLUG'] == 'APERTURE':
+        element['APERTURE'] = element['CIRCUIT']
     return element
 
 
 def aperture(ax, bl, **kwargs):
-
-    # if slits : use the context to define aperture
     if kwargs.get('context'):
         bl = bl.line.apply(lambda e: fill_aperture(e, kwargs.get('context')), axis=1)
     else:
@@ -112,16 +117,24 @@ def aperture(ax, bl, **kwargs):
 
     planes = kwargs.get('plane', 'both')
 
-    bl['APERTURE_UP'] = bl['APERTURE'].apply(lambda a: xy_from_string(a, planes == 'both' or planes == 'Y'))
-    bl['APERTURE_DOWN'] = bl['APERTURE'].apply(lambda a: xy_from_string(a, not (planes == 'both' or planes == 'X')))
+    bl['APERTURE_UP'] = bl['APERTURE'].apply(
+        lambda a: xy_from_string(a, planes == 'both' or planes == 'Y', kwargs.get('context'))
+    )
+    bl['APERTURE_DOWN'] = bl['APERTURE'].apply(
+        lambda a: xy_from_string(a, not (planes == 'both' or planes == 'X'), kwargs.get('context'))
+    )
 
     if 'CHAMBER' not in bl:
         bl['CHAMBER'] = 0
 
-    bl['CHAMBER_UP'] = bl['CHAMBER'].apply(lambda a: xy_from_string(a, planes == 'both' or planes == 'Y'))
-    bl['CHAMBER_DOWN'] = bl['CHAMBER'].apply(lambda a: xy_from_string(a, not (planes == 'both' or planes == 'X')))
+    bl['CHAMBER_UP'] = bl['CHAMBER'].apply(
+        lambda a: xy_from_string(a, planes == 'both' or planes == 'Y', kwargs.get('context'))
+    )
+    bl['CHAMBER_DOWN'] = bl['CHAMBER'].apply(
+        lambda a: xy_from_string(a, not (planes == 'both' or planes == 'X'), kwargs.get('context'))
+    )
 
     bl.query("CLASS == 'QUADRUPOLE'").apply(lambda e: draw_quad(ax, e), axis=1)
     bl.query("CLASS == 'SBEND'").apply(lambda e: draw_bend(ax, e), axis=1)
     bl.query("CLASS == 'RBEND'").apply(lambda e: draw_bend(ax, e), axis=1)
-    #bl.query("CLASS == 'COLLIMATOR'").apply(lambda e: draw_coll(ax, e, planes), axis=1)
+    bl.query("CLASS == 'COLLIMATOR'").apply(lambda e: draw_coll(ax, e, planes), axis=1)
