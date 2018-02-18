97ce3cc3e4ee0d152acdfa5ac7cf0dbd5431ce20
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2018-02-18 16:19:30 +0100

    Refactoring: `import georges` should not import the submodules; only the `core`.

--- __init__.py
+++ __init__.py
@@ -2,7 +2,5 @@ from .beam import Beam
 from .beamline import Beamline
 from .beamline_builder import BeamlineBuilder
 from .context import Context
-from . import experimental
 from . import physics
 from . import statistics
-from . import plotting
