aa4e5bf47e7188bed56ccdd5005d814653366d90
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2018-05-23 18:54:23 +0200

    Restore particle losses.

--- georges/manzoni/kick.py
+++ georges/manzoni/kick.py
@@ -64,6 +64,7 @@ def vkicker(e, b, **kwargs):
 def degrader(e, b, **kwargs):
     # Remove particles
     idx = np.random.randint(b.shape[0], size=int((1 - e[INDEX_FE_LOSS]) * b.shape[0]))
+    b = b[idx, :]
     b += nprandom.multivariate_normal(
         [0.0, 0.0, 0.0, 0.0, 0.0],
         np.array(
