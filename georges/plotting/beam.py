0ed4c71c52dac016205bd0bae3cebe9696b72d85
Unknown <robintesse@gmail.com>
2018-06-27 09:29:53 +0200

    New version for functions plotting and statistics

--- georges/plotting/beam.py
+++ georges/plotting/beam.py
@@ -1,7 +1,15 @@
 import numpy as np
 from numpy import linspace
 from matplotlib import patches
-from matplotlib.ticker import NullFormatter, MaxNLocator
+from matplotlib.ticker import NullFormatter
+from .. import statistics
+
+
+class BeamPlottingException(Exception):
+    """Exception raised for errors in the beam plotting module."""
+
+    def __init__(self, m):
+        self.message = m
 
 
 def ellipse(ra, rb, angle, x0, y0, **kwargs):
@@ -14,34 +22,19 @@ def ellipse(ra, rb, angle, x0, y0, **kwargs):
     :param y0: center Y coordinate
     :return: `matplotlib.patches.Ellipse` object
     """
-    theta = np.radians(np.arange(0.0, 360.0, 1.0))
 
     width = 2 * ra
     height = 2 * rb
 
-    x = width * np.cos(theta)
-    y = height * np.sin(theta)
-
-    rtheta = np.radians(angle)
-    rotation_matrix = np.array([
-        [np.cos(rtheta), -np.sin(rtheta)],
-        [np.sin(rtheta), np.cos(rtheta)],
-    ])
-
-    x, y = np.dot(rotation_matrix, np.array([x, y]))
-    x += x0
-    y += y0
-
     return patches.Ellipse((x0, y0), width, height,
-                           angle=angle,
+                           angle=np.rad2deg(angle),
                            linewidth=kwargs.get('linewidth', 1),
                            fill=kwargs.get('fill', False),
                            linestyle=kwargs.get('linestyle', '--'),
                            edgecolor=kwargs.get('color', 'black'))
 
 
-def draw2d_histo(fig, data, twiss_parameter, **kwargs):
-    draw_ellipse = kwargs.get('draw_ellipse', False)
+def phase_space(fig, data, **kwargs):
 
     # Define the x and y data
     x = data[data.columns[0]]
@@ -50,23 +43,26 @@ def draw2d_histo(fig, data, twiss_parameter, **kwargs):
     xlims = [min(x), max(x)]
     ylims = [min(y), max(y)]
 
-    # Set up default x and y limits
-    if kwargs.get('xlim') is not None:
-        xlims = kwargs.get('xlim')
+    if kwargs.get('twiss_angle') is None:
+        raise BeamPlottingException("Error no angle specified.")
 
-    if kwargs.get('ylim') is not None:
-        ylims = kwargs.get('ylim')
+    # Set up default x and y limits
+    if kwargs.get('xlims') is not None:
+        xlims = kwargs.get('xlims')
+
+    if kwargs.get('ylims') is not None:
+        ylims = kwargs.get('ylims')
 
     # Define the locations for the axes
-    left, width = 0.12, 0.55
-    bottom, height = 0.12, 0.55
+    left, width = 0.12, 0.6
+    bottom, height = 0.12, 0.6
     bottom_h = left_h = left + width + 0.05
 
     # Set up the geometry of the three plots
     rect_beam = [left, bottom, width, height]  # dimensions of temp plot
     rect_histx = [left, bottom_h, width, 0.25]  # dimensions of x-histogram
-    rect_histy = [left_h, bottom, 0.25, height]  # dimensions of y-histogram
-    rect_tab = [left_h, bottom_h, 0.25, 0.25]  # dimensions of tab
+    rect_histy = [left_h, bottom, 0.4, height]  # dimensions of y-histogram
+    rect_tab = [left_h, bottom_h, 0.4, 0.25]  # dimensions of tab
 
     # Make the three plots
     ax_global = fig.add_axes(rect_beam)  # beam plot
@@ -114,19 +110,14 @@ def draw2d_histo(fig, data, twiss_parameter, **kwargs):
     ycenter = np.mean(y)
     ra = np.std(x)
     rb = np.std(y)
-    ang = twiss_parameter['PHI']
+    ang = kwargs.get('twiss_angle')
 
-    if draw_ellipse:
+    if kwargs.get('draw_ellipse', False):
         ax_global.add_patch(ellipse(ra, rb, ang, xcenter, ycenter))
         ax_global.add_patch(ellipse(2 * ra, 2 * rb, ang, xcenter, ycenter))
 
-        r1 = (xcenter + ra + 0.25 * ra)
-        x1 = r1 * np.cos(np.deg2rad(ang))
-        y1 = r1 * np.sin(np.deg2rad(ang))
-
-        r2 = (xcenter + 2 * ra + 0.25 * ra)
-        x2 = r2 * np.cos(np.deg2rad(ang))
-        y2 = r2 * np.sin(np.deg2rad(ang))
+        x1, y1 = kwargs.get("xtext_pos")
+        x2, y2 = kwargs.get("ytext_pos")
 
         ax_global.annotate('$1\\sigma$', xy=(x1, y1), xytext=(x1, y1),
                            horizontalalignment='center', verticalalignment='center',
@@ -146,8 +137,8 @@ def draw2d_histo(fig, data, twiss_parameter, **kwargs):
     ybins = np.arange(ymin, ymax, (ymax - ymin) / nybins)
 
     # Plot the histograms
-    nx, binsx, _ = ax_histx.hist(x, bins=xbins, color='blue', histtype='step', normed=True)
-    ny, binsy, _ = ax_histy.hist(y, bins=ybins, orientation='horizontal', color='red', histtype='step', normed=True)
+    nx, binsx, _ = ax_histx.hist(x, bins=xbins, color='blue', histtype='step', normed=False)
+    ny, binsy, _ = ax_histy.hist(y, bins=ybins, orientation='horizontal', color='red', histtype='step', normed=False)
 
     # Set up the histogram limits
     ax_global.set_xlim(xlims)
@@ -156,42 +147,16 @@ def draw2d_histo(fig, data, twiss_parameter, **kwargs):
     ax_histy.set_ylim(ylims)
 
     if kwargs.get('gaussian_fit', False):
+
         limx = np.arange(xlims[0], xlims[1], (xlims[1] - xlims[0]) / nxbins)
-        bin_centerx, fitresults_x = stat.gaussian_fit(x, default_lim=False, lim=limx)
+        bin_centerx, fitresults_x = statistics.histogram_fit(x, bounds_binning=limx,
+                                                             verbose=kwargs.get("verbose", False))
         ax_histx.plot(bin_centerx, fitresults_x.best_fit, 'k--', linewidth=1)
 
         limy = np.arange(ylims[0], ylims[1], (ylims[1] - ylims[0]) / nybins)
-        bin_centery, fitresults_y = stat.gaussian_fit(y, default_lim=False, lim=limy)
+        bin_centery, fitresults_y = statistics.histogram_fit(y, bounds_binning=limy,
+                                                             verbose=kwargs.get("verbose", False))
         ax_histy.plot(fitresults_y.best_fit, bin_centery, 'k--', linewidth=1)
-
-        gaussian_meanx = float(fitresults_x.params['cen'].value)
-        gaussian_meany = float(fitresults_y.params['cen'].value)
-        gaussian_stdx = float(fitresults_x.params['wid'].value)
-        gaussian_stdy = float(fitresults_y.params['wid'].value)
-
-        gaussian_errormeanx = float(fitresults_x.params['cen'].stderr)
-        gaussian_errormeany = float(fitresults_y.params['cen'].stderr)
-        gaussian_errorstdx = float(fitresults_x.params['wid'].stderr)
-        gaussian_errorstdy = float(fitresults_y.params['wid'].stderr)
-
-        relative_error_meanx = 100 * gaussian_errormeanx / gaussian_meanx
-        relative_error_meany = 100 * gaussian_errormeany / gaussian_meany
-        relative_error_stdx = 100 * gaussian_errorstdx / gaussian_stdx
-        relative_error_stdy = 100 * gaussian_errorstdy / gaussian_stdy
-
-        print(f"GAUSSIAN MEAN X: {gaussian_meanx:{10}.{3}}"
-              f"+/- {gaussian_errormeanx:{10}.{3}}"
-              f"({relative_error_meanx:{10}.{3}}%)\n"
-              f"GAUSSIAN STD X: {gaussian_stdx:{10}.{3}}"
-              f"+/- {gaussian_errorstdx:{10}.{3}}"
-              f"({relative_error_stdx:{10}.{3}}%)\n"
-              f"GAUSSIAN MEAN Y: {gaussian_meany:{10}.{3}}"
-              f"+/- {gaussian_errormeany:{10}.{3}}"
-              f"({relative_error_meany:{10}.{3}}%)\n"
-              f"GAUSSIAN STD Y: {gaussian_stdy:{10}.{3}}"
-              f"+/- {gaussian_errorstdy:{10}.{3}}"
-              f"({relative_error_stdy:{10}.{3}}%)\n"
-              )
 
     if kwargs.get('draw_tab', False):
 
@@ -218,13 +183,13 @@ def draw2d_histo(fig, data, twiss_parameter, **kwargs):
 
         ax_tab.annotate('mean', xy=(x0 + 0.375, y0 + 0.9), xytext=(x0 + 0.375, y0 + 0.9),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
-        ax_tab.annotate('std', xy=(x0 + 0.625, y0 + 0.9), xytext=(x0 + 0.625, y0 + 0.9),
+                        fontsize=12)
+        ax_tab.annotate('std ', xy=(x0 + 0.625, y0 + 0.9), xytext=(x0 + 0.625, y0 + 0.9),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
         ax_tab.annotate('median', xy=(x0 + 0.875, y0 + 0.9), xytext=(x0 + 0.875, y0 + 0.9),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
 
         xname = '0'
         yname = '1'
@@ -235,184 +200,28 @@ def draw2d_histo(fig, data, twiss_parameter, **kwargs):
 
         ax_tab.annotate(xname, xy=(x0 + 0.125, 0.6), xytext=(x0 + 0.125, 0.6),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
 
         ax_tab.annotate(yname, xy=(x0 + 0.125, 0.2), xytext=(x0 + 0.125, 0.2),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
 
         ax_tab.annotate(mean_x, xy=(x0 + 0.375, 0.6), xytext=(x0 + 0.375, 0.6),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
         ax_tab.annotate(std_x, xy=(x0 + 0.625, 0.6), xytext=(x0 + 0.625, 0.6),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
         ax_tab.annotate(median_x, xy=(x0 + 0.875, 0.6), xytext=(x0 + 0.875, 0.6),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
 
         ax_tab.annotate(mean_y, xy=(x0 + 0.375, 0.2), xytext=(x0 + 0.375, 0.2),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
         ax_tab.annotate(std_y, xy=(x0 + 0.625, 0.2), xytext=(x0 + 0.625, 0.2),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
+                        fontsize=12)
         ax_tab.annotate(median_y, xy=(x0 + 0.875, 0.2), xytext=(x0 + 0.875, 0.2),
                         horizontalalignment='center', verticalalignment='center',
-                        fontsize=6)
-
-
-
-# Define a function to make the ellipses
-def ellipse(ra, rb, ang, x0, y0, Nb=100):
-    xpos, ypos = x0, y0
-    radm, radn = ra, rb
-    an = ang
-    co, si = np.cos(an), np.sin(an)
-    the = linspace(0, 2 * np.pi, Nb)
-    X = radm * np.cos(the) * co - si * radn * np.sin(the) + xpos
-    Y = radm * np.cos(the) * si + co * radn * np.sin(the) + ypos
-    return X, Y
-
-
-def make2Dplot(fig, Data_BEAMX, Data_BEAMY, Nbinx, Nbiny):
-    # Define the x and y data
-    # For example just using random numbers
-
-    x = Data_BEAMX
-    y = Data_BEAMY
-
-    # Set up default x and y limits
-    xlims = [min(x), max(x)]
-    ylims = [min(y), max(y)]
-
-    # Define the locations for the axes
-    left, width = 0.12, 0.55
-    bottom, height = 0.12, 0.55
-    bottom_h = left_h = left + width + 0.02
-
-    # Set up the geometry of the three plots
-    rect_beam = [left, bottom, width, height]  # dimensions of temp plot
-    rect_histx = [left, bottom_h, width, 0.25]  # dimensions of x-histogram
-    rect_histy = [left_h, bottom, 0.25, height]  # dimensions of y-histogram
-
-    # Make the three plots
-    axBeam = fig.add_axes(rect_beam)  # beam plot
-    mean_DataX = str(round(Data_BEAMX.mean(), 3))
-    std_DataX = str(round(Data_BEAMX.std(), 3))
-    mean_DataY = str(round(Data_BEAMY.mean(), 3))
-    std_DataY = str(round(Data_BEAMY.std(), 3))
-
-    axHistx = fig.add_axes(rect_histx)  # x histogram
-    axHistx.set_ylabel("Counts")
-    axHistx.set_title('Mean : ' + mean_DataX + ' std : ' + std_DataX)
-    axHistx.grid(True)
-
-    axHisty = fig.add_axes(rect_histy)  # y histogram
-    axHisty.set_xlabel("Counts")
-    axHisty.set_title('Mean : ' + mean_DataY + ' std : ' + std_DataY, rotation=270, x=1.08, y=0.75)
-    axHisty.grid(True)
-
-    # Remove the inner axes numbers of the histograms
-    nullfmt = NullFormatter()
-    axHistx.xaxis.set_major_formatter(nullfmt)
-    axHisty.yaxis.set_major_formatter(nullfmt)
-
-    # Find the min/max of the data
-    xmin = min(xlims)
-    xmax = max(xlims)
-    ymin = min(ylims)
-    ymax = max(ylims)
-
-    # Make the 'main' beam plot
-    # Define the number of bins
-    nxbins = Nbinx
-    nybins = Nbiny
-
-    xbins = linspace(start=xmin, stop=xmax, num=nxbins)
-    ybins = linspace(start=ymin, stop=ymax, num=nybins)
-    xcenter = (xbins[0:-1] + xbins[1:]) / 2.0
-    ycenter = (ybins[0:-1] + ybins[1:]) / 2.0
-    aspectratio = 1.0 * (xmax - 0) / (1.0 * ymax - 0)
-
-    H, xedges, yedges = np.histogram2d(y, x, bins=(ybins, xbins))
-    X = xcenter
-    Y = ycenter
-    Z = H
-
-    # Plot the beam data
-    cax = (axBeam.imshow(H, extent=[xmin, xmax, ymin, ymax],
-                         interpolation='nearest', origin='lower', aspect='auto'))
-
-    print('xmin : ' + str(xmin) + ' xmax : ' + str(xmax) + ' ymin: ' + str(ymin) + ' ymax: ' + str(ymax))
-    # Plot the beam plot contours
-    contourcolor = 'white'
-    xcenter = np.mean(x)
-    ycenter = np.mean(y)
-    ra = np.std(x)
-    rb = np.std(y)
-    ang = 0  ##To change for rotated ellipse : call georges.phys
-
-    X, Y = ellipse(ra, rb, ang, xcenter, ycenter)
-    axBeam.plot(X, Y, "k:", ms=1, linewidth=2.0)
-    axBeam.annotate('$1\\sigma$', xy=(X[15], Y[15]), xycoords='data', xytext=(10, 10),
-                    textcoords='offset points', horizontalalignment='right',
-                    verticalalignment='bottom', fontsize=25)
-
-    # X,Y=ellipse(2*ra,2*rb,ang,xcenter,ycenter)
-    # axBeam.plot(X,Y,"k:",color = contourcolor,ms=1,linewidth=2.0)
-    # axBeam.annotate('$2\\sigma$', xy=(X[15], Y[15]), xycoords='data',xytext=(10, 10),
-    #             textcoords='offset points',horizontalalignment='right',
-    #             verticalalignment='bottom',fontsize=25, color = contourcolor)
-
-    X, Y = ellipse(3 * ra, 3 * rb, ang, xcenter, ycenter)
-    axBeam.plot(X, Y, "k:", color=contourcolor, ms=1, linewidth=2.0)
-    axBeam.annotate('$3\\sigma$', xy=(X[15], Y[15]), xycoords='data', xytext=(10, 10),
-                    textcoords='offset points', horizontalalignment='right',
-                    verticalalignment='bottom', fontsize=25, color=contourcolor)
-
-    # Plot the axes labels
-    axBeam.set_xlabel(Data_BEAMX.name, fontsize=25)
-    axBeam.set_ylabel(Data_BEAMY.name, fontsize=25)
-
-    # Make the tickmarks pretty
-    ticklabels = axBeam.get_xticklabels()
-    for label in ticklabels:
-        label.set_fontsize(18)
-        label.set_family('serif')
-
-    ticklabels = axBeam.get_yticklabels()
-    for label in ticklabels:
-        label.set_fontsize(18)
-        label.set_family('serif')
-
-    # Set up the plot limits
-    axBeam.set_xlim(xlims)
-    axBeam.set_ylim(ylims)
-
-    # Set up the histogram bins
-    xbins = np.arange(xmin, xmax, (xmax - xmin) / nxbins)
-    ybins = np.arange(ymin, ymax, (ymax - ymin) / nybins)
-
-    # Plot the histograms
-    axHistx.hist(x, bins=xbins, color='blue', histtype='step', normed=True)
-    axHisty.hist(y, bins=ybins, orientation='horizontal', color='red', histtype='step', normed=True)
-    # Set up the histogram limits
-    axHistx.set_xlim(min(x), max(x))
-    axHisty.set_ylim(min(y), max(y))
-
-    # Make the tickmarks pretty
-    ticklabels = axHistx.get_yticklabels()
-    for label in ticklabels:
-        label.set_fontsize(12)
-        label.set_family('serif')
-
-    # Make the tickmarks pretty
-    ticklabels = axHisty.get_xticklabels()
-    for label in ticklabels:
-        label.set_fontsize(12)
-        label.set_family('serif')
-
-    # Cool trick that changes the number of tickmarks for the histogram axes
-    axHisty.xaxis.set_major_locator(MaxNLocator(4))
-    axHistx.yaxis.set_major_locator(MaxNLocator(4))
+                        fontsize=12)
--- georges/statistics.py
+++ georges/statistics.py
@@ -1,63 +1,39 @@
 """ Compute some functions for statistics as confidence bounds"""
 import numpy as np
-from lmfit import Model
+from lmfit.models import GaussianModel
+from lmfit.models import LorentzianModel
 
 
-def gaussian(x, amp, cen, wid):
-    """1-d gaussian: gaussian(x, amp, cen, wid)"""
-    return (amp/(np.sqrt(2*np.pi)*wid)) * np.exp(-(x-cen)**2 / (2*wid**2))
+REGISTERED_MODEL = ['Gaussian', 'Lorentzian']
 
 
-def gaussian_fit(data, **kwargs):
+class StatisticException(Exception):
+    """Exception raised for errors in the beam plotting module."""
 
-    if kwargs.get('lim') is None:
-        lim = 50
-    else:
-        lim = kwargs.get('lim')
+    def __init__(self, m):
+        self.message = m
 
-    y, bin_edges = np.histogram(data, density=True, bins=lim)
+
+def histogram_fit(data, bounds_binning=50, verbose=False, **kwargs):
+
+    """ All models are available on https://lmfit.github.io/lmfit-py/builtin_models.html#lmfit.models"""
+    y, bin_edges = np.histogram(data, density=False, bins=bounds_binning)
     x = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-    gmodel = Model(gaussian)
-    result = gmodel.fit(y, x=x, amp=1, cen=x.mean(), wid=x.var())
+    model = kwargs.get("model", "Gaussian")
 
-    if kwargs.get('fit_report', False):
+    if model not in REGISTERED_MODEL:
+        # TODO make an excception
+        raise StatisticException(f"The model {model} is not in {REGISTERED_MODEL}")
+
+    if model == "Gaussian":
+        mod = GaussianModel()
+
+    if model == "Lorentzian":
+        mod = LorentzianModel()
+
+    result = mod.fit(data=y, x=x)
+
+    if verbose:
         print(result.fit_report())
-
     return x, result
-
-#
-# def gaussian_fit(data, nbin=50):
-#
-#     data_bin = np.arange(data.min(), data.max(), (data.max() - data.min()) / nbin)
-#     n, bins = np.histogram(data, bins=data_bin, normed=True)
-#     bincenter = (bins[:-1] + bins[1:]) / 2
-#     coeff, var_matrix = curve_fit(gauss, bincenter, n, p0=[1, data.mean(), data.std()], maxfev=1000)
-#     hist_fit = gauss(bincenter, *coeff)
-#     error = np.sqrt(np.diag(var_matrix))
-#
-#     return bincenter, coeff, hist_fit, error
-
-###################################################################
-
-# def mean_confidence_interval(data, confidence=0.95):
-#     a = 1.0*np.array(data)
-#     n = len(a)
-#     m, se = np.mean(a), scipy.stats.sem(a)
-#     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
-#     return m, m-h, m+h
-#
-#
-# def std_confidence_interval(data, alpha=0.05):
-#
-#     n=len(data)
-#     s2=np.var(data)
-#
-#     chi2min=chi2.isf(alpha/2, n-1)
-#     chi2max=chi2.isf(1-alpha/2, n-1)
-#
-#
-#     confidence_min=np.sqrt((n-1)*s2/chi2min)
-#     confidence_max=np.sqrt((n-1)*s2/chi2max)
-#
-#     return confidence_min,confidence_max
