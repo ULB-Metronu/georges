72a0a6eec35bcc8f7c0e76a20f1542083691a3f3
Robin Tesse <robintesse@gmail.com>
2018-10-12 08:54:12 +0200

    Add material to database

--- georges/bdsim/material_database.py
+++ georges/bdsim/material_database.py
@@ -7,3 +7,9 @@ def get_bdsim_material(mat_name):
 
     if mat_name == 'lexan':
         return 'G4_POLYCARBONATE'
+
+    if mat_name == 'polyethylen':
+        return 'G4_POLYETHYLENE'
+
+    if mat_name == 'brass':
+        return 'G4_BRASS'
