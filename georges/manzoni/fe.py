6581aabb91a8d6da31c9760eeba4cd8ca116f9e3
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-03-29 14:45:21 +0100

    Bug fix in Manzoni/Fermi propagation.

--- georges/manzoni/fe.py
+++ georges/manzoni/fe.py
@@ -8,22 +8,22 @@ except ModuleNotFoundError:
 
 
 def propagation_scatterer(e, b, **kwargs):
-    b[X, X] += e[INDEX_FE_A2] + b[X, PX] * e[INDEX_LENGTH] + b[1, 1] * e[INDEX_LENGTH]**2
-    b[X, PX] += e[INDEX_FE_A1] + b[PX, PX] * e[INDEX_LENGTH]
-    b[PX, X] = b[X, PX]
     b[PX, PX] += e[INDEX_FE_A0]
-    
-    b[Y, Y] += e[INDEX_FE_A2] + b[Y, PY] * e[INDEX_LENGTH] + b[1, 1] * e[INDEX_LENGTH]**2
-    b[Y, PY] += e[INDEX_FE_A1] + b[PY, PY] * e[INDEX_LENGTH]
-    b[PY, Y] = b[Y, PY]
     b[PY, PY] += e[INDEX_FE_A0]
-
     return b
 
 
 def propagation_degrader(e, b, **kwargs):
+    b[X, X] += e[INDEX_FE_A2] + b[X, PX] * e[INDEX_LENGTH] + b[PX, PX] * e[INDEX_LENGTH] ** 2
+    b[X, PX] += e[INDEX_FE_A1] + b[PX, PX] * e[INDEX_LENGTH]
+    b[PX, X] = b[X, PX]
     b[PX, PX] += e[INDEX_FE_A0]
+
+    b[Y, Y] += e[INDEX_FE_A2] + b[Y, PY] * e[INDEX_LENGTH] + b[PY, PY] * e[INDEX_LENGTH] ** 2
+    b[Y, PY] += e[INDEX_FE_A1] + b[PY, PY] * e[INDEX_LENGTH]
+    b[PY, Y] = b[Y, PY]
     b[PY, PY] += e[INDEX_FE_A0]
+
     return b
 
 
@@ -38,6 +38,18 @@ def mc_degrader(e, b, **kwargs):
     # if e[INDEX_FE_LOSS] != 0:
     #     idx = np.random.randint(b.shape[0], size=int((e[INDEX_FE_LOSS]) * b.shape[0]))
     #     b = b[idx, :]
+    # Transport
+    matrix = np.array(
+        [
+            [1, e[INDEX_LENGTH], 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, e[INDEX_LENGTH], 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1]
+        ]
+    )
+    b = b.dot(matrix.T)
+    # Interactions
     b += nprandom.multivariate_normal(
         [0.0, 0.0, 0.0, 0.0, 0.0],
         np.array(
