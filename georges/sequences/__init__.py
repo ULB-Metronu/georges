403a99d1404222a842b4aad4e4f0f27c045fe7fe
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2019-07-21 08:52:09 +0200

    Moving the sequence module to georges-core
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges/sequences/__init__.py
+++
@@ -1,4 +0,0 @@
-"""TODO"""
-from .elements import Element, ElementClass
-from .sequence import SequenceMetadata, Sequence, PlacementSequence, TwissSequence, SequenceException
-from .betablock import BetaBlock, BetaBlockType
