4403e0fa69038b39ea2ca178329280bb2cd90cb6
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2019-09-02 11:26:09 +0200

    Feature - Tracking observers
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges/manzoni/core.py
+++ georges/manzoni/core.py
@@ -1,15 +1,28 @@
 from __future__ import annotations
+from typing import Optional
 import numpy as _np
 from .input import Input as _Input
+from .observers import Observer
 
 
-def track(beamline: _Input, beam: _np.ndarray):
+def track(beamline: _Input, beam: _np.ndarray, observer: Optional[Observer]):
+    """
+
+    Args:
+        beamline:
+        beam:
+        observer:
+
+    Returns:
+
+    """
     b1 = beam  # .copy()
     b2 = _np.zeros(beam.shape)
     for e in beamline.sequence:
         b1, b2 = e.propagate(b1, b2)
         b1, b2 = e.aperture(b1, b2)
         b2 = _np.zeros(b1.shape)
+        observer(e, b1, b2)
 
 
 def twiss():
---
+++ georges/manzoni/observers.py
@@ -0,0 +1,30 @@
+"""
+TODO
+"""
+import numpy as _np
+import pandas as _pd
+
+
+class ObserverType(type):
+    pass
+
+
+class Observer(metaclass=ObserverType):
+    def __init__(self):
+        self.data = {}
+
+    def __call__(self, element, b1, b2):
+        pass
+
+    def to_df(self) -> _pd.DataFrame:
+        return _pd.DataFrame(self.data)
+
+
+class BeamObserver:
+    def __call__(self, element, b1, b2):
+        self.data[element.LABEL1] = (b1, b2)
+
+
+class SigmaObserver:
+    def __call__(self, element, b1, b2):
+        self.data[element.LABEL1] = _np.array()
