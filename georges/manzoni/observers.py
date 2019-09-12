de436ae45fb868a8217b964cea775eb086b5b120
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2019-09-12 12:12:39 +0200

    WIP observers
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges/manzoni/observers.py
+++ georges/manzoni/observers.py
@@ -11,20 +11,33 @@ class ObserverType(type):
 
 class Observer(metaclass=ObserverType):
     def __init__(self):
-        self.data = {}
+        self.data = []
+        self.headers = []
 
     def __call__(self, element, b1, b2):
         pass
 
     def to_df(self) -> _pd.DataFrame:
-        return _pd.DataFrame(self.data)
+        return _pd.DataFrame(self.data, columns=self.headers)
 
 
 class BeamObserver(Observer):
+    def __init__(self):
+        super().__init__()
+        self.headers = ('LABEL1', 'BEAM_OUT', 'BEAM_IN')
+
     def __call__(self, element, b1, b2):
-        self.data[element.LABEL1] = (b1, b2)
+        self.data.append((element.LABEL1, b1, b2))
 
 
 class SigmaObserver(Observer):
+    def __init__(self):
+        super().__init__()
+        self.headers = ('LABEL1', 'BEAM_OUT_X', 'BEAM_IN')
+
     def __call__(self, element, b1, b2):
-        self.data[element.LABEL1] = _np.array()
+        self.data.append((element.LABEL1, b1[0].std(), b2[0].std()))
+
+
+class LossesObserver(Observer):
+    pass
