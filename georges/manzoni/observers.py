18315d93ea5a2e8bc5d94f7d365c37697f0e7cf3
avdhoeke <arthur.vandenhoeke@ulb.be>
2019-10-14 15:11:20 +0200

    Feature - SigmaObserver and MeanObserver

--- georges/manzoni/observers.py
+++ georges/manzoni/observers.py
@@ -31,16 +31,65 @@ class BeamObserver(Observer):
 
 
 class MeanObserver(Observer):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.headers = ('LABEL1',
+                        'BEAM_IN_X',
+                        'BEAM_OUT_X',
+                        'BEAM_IN_Y',
+                        'BEAM_OUT_Y',
+                        'BEAM_IN_XP',
+                        'BEAM_OUT_XP',
+                        'BEAM_IN_YP',
+                        'BEAM_OUT_YP',
+                        'BEAM_IN_DPP',
+                        'BEAM_OUT_DPP',
+                        )
+
+    def __call__(self, element, b1, b2):
+        self.data.append((element.LABEL1,
+                          b1[0].mean(),
+                          b2[0].mean(),
+                          b1[2].mean(),
+                          b2[2].mean(),
+                          b1[1].mean(),
+                          b2[1].mean(),
+                          b1[3].mean(),
+                          b2[3].mean(),
+                          b1[4].mean(),
+                          b2[4].mean(),
+                          ))
 
 
 class SigmaObserver(Observer):
     def __init__(self):
         super().__init__()
-        self.headers = ('LABEL1', 'BEAM_OUT_X', 'BEAM_IN')
+        self.headers = ('LABEL1',
+                        'BEAM_IN_X',
+                        'BEAM_OUT_X',
+                        'BEAM_IN_Y',
+                        'BEAM_OUT_Y',
+                        'BEAM_IN_XP',
+                        'BEAM_OUT_XP',
+                        'BEAM_IN_YP',
+                        'BEAM_OUT_YP',
+                        'BEAM_IN_DPP',
+                        'BEAM_OUT_DPP',
+                        )
 
     def __call__(self, element, b1, b2):
-        self.data.append((element.LABEL1, b1[0].std(), b2[0].std()))
+        self.data.append((element.LABEL1,
+                          b1[0].std(),
+                          b2[0].std(),
+                          b1[2].std(),
+                          b2[2].std(),
+                          b1[1].std(),
+                          b2[1].std(),
+                          b1[3].std(),
+                          b2[3].std(),
+                          b1[4].std(),
+                          b2[4].std(),
+                          ))
 
 
 class LossesObserver(Observer):
