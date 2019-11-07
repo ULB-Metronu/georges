78c7face10f455470cb962a09a33326fb88347c7
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2019-11-07 13:59:29 +0100

    Improve georges.bdsim.output to support branches in the Event tree.
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges/bdsim/output.py
+++ georges/bdsim/output.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+from typing import Optional
 import logging
 import os
 try:
@@ -30,8 +32,20 @@ class Output(metaclass=OutputType):
             """
             self._tree = tree
 
+        @property
+        def numentries(self):
+            return self._tree.numentries
+
     class _Branch:
-        pass
+        def __init__(self, tree: Output._Tree):
+            self._tree: Output._Tree = tree
+            self._df: Optional[_pd.DataFrame] = None
+
+        @property
+        def df(self):
+            if self._df is None:
+                return self.read_df()
+            return self._df
 
 
 class BDSimOutput(Output):
@@ -85,7 +99,7 @@ class BDSimOutput(Output):
 
     class Model(Output._Tree):
 
-        def extract_geometry(self) -> _pd.DataFrame:
+        def read(self) -> _pd.DataFrame:
             """
 
             Returns:
@@ -97,7 +111,7 @@ class BDSimOutput(Output):
             for branch, name in {'Model.componentName': 'NAME',
                                  'Model.componentType': 'TYPE',
                                  'Model.material': 'MATERIAL'}.items():
-                data = [_.decode('utf-8') for _ in self._branch.array(branch=[branch])[0]]
+                data = [_.decode('utf-8') for _ in self._tree.array(branch=[branch])[0]]
                 model_geometry_df[name] = data
 
             # Scalar
@@ -105,13 +119,13 @@ class BDSimOutput(Output):
                                  'Model.staS': 'AT_ENTRY',
                                  'Model.midS': 'AT_CENTER',
                                  'Model.endS': 'AT_EXIT'}.items():
-                model_geometry_df[name] = self._branch.array(branch=[branch])[0]
+                model_geometry_df[name] = self._tree.array(branch=[branch])[0]
 
             # Vectors
             geometry_branches = {'Model.staPos': 'ENTRY_',
                                  'Model.midPos': 'CENTER_',
                                  'Model.endPos': 'EXIT_'}
-            data = self._branch.pandas.df(branches=geometry_branches.keys(), flatten=True)
+            data = self._tree.pandas.df(branches=geometry_branches.keys(), flatten=True)
             for branch, name in geometry_branches.items():
                 data.rename({f"{branch}.fX": f"{name}X", f"{branch}.fY": f"{name}Y", f"{branch}.fZ": f"{name}Z"},
                             axis='columns', inplace=True)
@@ -120,33 +134,30 @@ class BDSimOutput(Output):
             return _pd.concat([model_geometry_df, data.loc[0]], axis='columns', sort=False).set_index('NAME')
 
     class Run(Output._Tree):
-        def __init__(self):
-            super().__init__()
+        def __init__(self, tree):
+            super().__init__(tree)
 
         class Summary(Output._Branch):
             def __init__(self):
                 pass
 
     class Event(Output._Tree):
-        def __init__(self):
-            super().__init__()
-            self._eloss = BDSimOutput.Event.ELoss()
-            self._eloss_vacuum = BDSimOutput.Event.ELossVacuum()
-            self._eloss_tunnel = BDSimOutput.Event.ELossTunnel()
-            self._eloss_world = BDSimOutput.Event.ELossWorld()
-            self._eloss_world_exit = BDSimOutput.Event.ELossWorldExit()
-
-        @property
-        def eloss(self):
-            return self._eloss
+        def __init__(self, tree):
+            super().__init__(tree)
+            self.eloss = BDSimOutput.Event.ELoss(self)
+            self.eloss_vacuum = BDSimOutput.Event.ELossVacuum(self)
+            self.eloss_tunnel = BDSimOutput.Event.ELossTunnel(self)
+            self.eloss_world = BDSimOutput.Event.ELossWorld(self)
+            self.eloss_world_exit = BDSimOutput.Event.ELossWorldExit(self)
+            self.primary_first_hit = BDSimOutput.Event.PrimaryFirstHit(self)
+            self.primary_last_hit = BDSimOutput.Event.PrimaryLastHit(self)
+            self.aperture_impacts = BDSimOutput.Event.ApertureImpacts(self)
 
         class ELoss(Output._Branch):
-            def __init__(self):
-                pass
+            pass
 
         class ELossVacuum(Output._Branch):
-            def __init__(self):
-                pass
+            pass
 
         class ELossTunnel(Output._Branch):
             pass
@@ -158,13 +169,66 @@ class BDSimOutput(Output):
             pass
 
         class PrimaryFirstHit(Output._Branch):
-            pass
+            def read_df(self) -> _pd.DataFrame:
+                self._df = self._tree._tree.pandas.df(branches=tuple(map(lambda _: f"{self.__class__.__name__}.{_}", (
+                    'n',
+                    'S',
+                    'weight',
+                    #'partID',
+                    #'trackID',
+                    #'parentID',
+                    'modelID',
+                    'turn',
+                    'x',
+                    'y',
+                    'z',
+                    'X',
+                    'Y',
+                    'Z',
+                    'T',
+                    #'stepLength',
+                    #'preStepKineticEnergy',
+                    'storeTurn',
+                    'storeLinks',
+                    'storeModelID',
+                    'storeLocal',
+                    'storeGlobal',
+                    'storeTime',
+                    'storeStepLength',
+                    'storePreStepKineticEnergy',
+                ))))
+                self._df.columns = self._df.columns.str.lstrip(self.__class__.__name__ + '.')
+                return self._df
 
-        class PrimaryLastHit(Output._Branch):
+        class PrimaryLastHit(PrimaryFirstHit):
             pass
 
         class ApertureImpacts(Output._Branch):
-            pass
+            def read_df(self) -> _pd.DataFrame:
+                self._df = self._tree._tree.pandas.df(branches=tuple(map(lambda _: f"{self.__class__.__name__}.{_}", (
+                    'n',
+                    'energy',
+                    'S',
+                    'weight',
+                    'isPrimary',
+                    'firstPrimaryImpact',
+                    'partID',
+                    'turn',
+                    'x',
+                    'y',
+                    'xp',
+                    'yp',
+                    'T',
+                    'kineticEnergy',
+                    'isIon',
+                    'ionA',
+                    'ionZ',
+                    'trackID',
+                    'parentID',
+                    'modelID',
+                ))))
+                self._df.columns = self._df.columns.str.lstrip(self.__class__.__name__ + '.')
+                return self._df
 
 
 class ReBDSimOutput(Output):
