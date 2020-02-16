69ff91ff1adadc8fb9e9c010bda09c4a619ff93e
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-02-16 14:24:02 +0100

    Bug fix for the thin scatterers physics.

--- georges/manzoni/fe.py
+++ georges/manzoni/fe.py
@@ -28,8 +28,8 @@ def propagation_degrader(e, b, **kwargs):
 
 
 def mc_scatterer(e, b, **kwargs):
-    b[:, 1] += nprandom.normal(0.0, e[INDEX_FE_A0], int(b.shape[0]))
-    b[:, 3] += nprandom.normal(0.0, e[INDEX_FE_A0], int(b.shape[0]))
+    b[:, 1] += nprandom.normal(0.0, np.sqrt(e[INDEX_FE_A0]), int(b.shape[0]))
+    b[:, 3] += nprandom.normal(0.0, np.sqrt(e[INDEX_FE_A0]), int(b.shape[0]))
     return b
 
 
