5169416d0e8c196709ebb308efbd0ff862cf476e
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-03-31 10:02:09 +0200

    Feature - Scatterers now take into account the losses and energy spread.
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
    
    Add data for beryllium
    
    Change density of diamond
    
    Use a third order fit for transmission
    
    Fix for degrader
    
    
    Typing
    
    Add fit for diamond
    
    Add fit data for materials
    
    Fix minor bugs in the tracking and creates a Gap element for Manzoni (drift with no aperture check).
    
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
    
    Add a scatterer element for Manzoni.
    
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges/manzoni/elements/scatterers.py
+++ georges/manzoni/elements/scatterers.py
@@ -13,14 +13,32 @@ from .elements import ManzoniElement as _ManzoniElement
 from ...fermi import materials
 
 
-class MaterialElement(_ManzoniElement):
-    INTEGRATOR = None
+class Scatterer(_ManzoniElement):
+    PARAMETERS = {
+        'MATERIAL': (materials.Vacuum, 'Degrader material'),
+        'KINETIC_ENERGY': (82.5 * _ureg.MeV, 'Incoming beam energy'),
+    }
+    """Parameters of the element, with their default value and their descriptions."""
 
     @property
-    def cache(self) -> list:
-        if not self.frozen:
-            self._cache = self.parameters
-        return self._cache
+    def parameters(self) -> List[float]:
+        fe = self.MATERIAL.scattering(kinetic_energy=self.KINETIC_ENERGY, thickness=self.L)
+        return [
+            fe['A'][0],
+        ]
+
+    def propagate(self,
+                  beam_in: _np.ndarray,
+                  beam_out: _np.ndarray = None,
+                  global_parameters: list = None,
+                  ) -> Tuple[_np.ndarray, _np.ndarray]:
+        a0 = self.parameters[0]
+
+        beam_out[:, 1] = beam_in[:, 1] + nprandom.normal(0.0, a0, size=beam_in.shape[0])
+        beam_out[:, 3] = beam_in[:, 3] + nprandom.normal(0.0, a0, size=beam_in.shape[0])
+
+        return beam_in, beam_out
+
 
 class Degrader(_ManzoniElement):
     # TODO use the real energy of the beam
@@ -92,7 +110,7 @@ class Degrader(_ManzoniElement):
         return beam_in, beam_out
 
 
-class BeamStop(MaterialElement):
+class BeamStop(_ManzoniElement):
     PARAMETERS = {
         'MATERIAL': (materials.Lead, 'Beam Stop material'),
         'L': (0.0 * _ureg.m, 'Beam Stop length'),
