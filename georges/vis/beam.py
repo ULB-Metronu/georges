be784057da137749b7b98b0474867c421208c63c
Eliott Ramoisiaux <Eliott.ramoisiaux@ulb.be>
2020-05-08 17:03:21 +0200

    Add minor changes

--- georges/vis/beam.py
+++ georges/vis/beam.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import patches
 from matplotlib.ticker import NullFormatter
-#from georges import statistics
+from . import statistics
 
 
 class BeamPlottingException(Exception):
