287397eb7b6e9f193c668660948a089601a06c8c
Eustache <eustache.gnacadja@ulb.be>
2020-10-29 18:00:17 +0100

    Reformat georges.vis: Add matplotlib.py and clean-up

--- georges/vis/__init__.py
+++ georges/vis/__init__.py
@@ -1,12 +1,2 @@
-from georges_core.vis import *
-from .common import prepare, draw_beamline
-from .aperture import aperture
-from .twiss import twiss
-from .twiss import beta, alpha, dispersion, phase_advance
-from .losses import losses
-from .tracking import tracking
-from .survey import survey
-from .bpm import bpm
-from .scattering import scattering
-from .beam import phase_space, five_spot_map
-from .summary import summary, tracking_summary
+from .matplotlib import ManzoniMatplotlibArtist
+
