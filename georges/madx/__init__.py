e3f5cb7d159b0f13db3a13676a76805537ec43fd
Robin Tesse <robin.tesse@ulb.be>
2022-03-04 15:33:47 +0100

    Add test for madx/cpymad

--- georges/madx/__init__.py
+++ georges/madx/__init__.py
@@ -4,5 +4,4 @@ try:
     import cpymad
 except ImportError:
     logging.getLogger(__name__).warning("cpymad is not present, this module is loaded with reduced functionalities.")
-    pass
 from .madx import MadX
